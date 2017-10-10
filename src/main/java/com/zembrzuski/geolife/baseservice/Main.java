a886885c4444c6d7a0fde4ed1ea22bfc06e57d1b
Rodrigo Zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2017-10-10 09:38:58 -0300

    nao sei, mas deve estar bom

--- src/main/java/com/zembrzuski/geolife/baseservice/Main.java
+++ src/main/java/com/zembrzuski/geolife/baseservice/Main.java
@@ -2,9 +2,17 @@ package com.zembrzuski.geolife.baseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Main {
+
+	@Bean
+	public RestTemplate verrySimpleRestTemplateBuilder() {
+		return new RestTemplateBuilder().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
--- src/main/java/com/zembrzuski/geolife/baseservice/ZembrzuskiSimpleController.java
+++ src/main/java/com/zembrzuski/geolife/baseservice/ZembrzuskiSimpleController.java
@@ -1,29 +1,63 @@
 package com.zembrzuski.geolife.baseservice;
 
 import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
-@Controller
+@Configuration
+@RequestMapping(value = "/track")
 public class ZembrzuskiSimpleController {
 
-    @RequestMapping(value = "/zembrzuski/{myParam}", method = RequestMethod.GET)
-    public ResponseEntity<Map<String, String>> persistPerson(@PathVariable String myParam) {
+    @Value("${elasticsearch_url}")
+    private String elasticsearchUrl;
 
+    @Value("${find_by_term}")
+    private String findByTerm;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * TODO continuar implementando essa classe.
+     * Esse endpoint ainda não está filtrando pelo trackId
+     *
+     * Depois, tenho que criar o endpoint que dá todas as rotas de um determinado usuário.
+     *
+     * Depois, tenho que fazer alguma parada com visualização de informações. Para, de fato,
+     * ver se as rotas se repetem.
+     *
+     * Por fim, acho justo de implementar as coisas nos artigos que lerei.
+     *
+     * Agora, vou ler um artigo.
+     */
+
+    @RequestMapping(value = "/by-id/{trackId}", method = RequestMethod.GET)
+    public ResponseEntity<Map<String, Object>> retrieveTrackById(@PathVariable String trackId) {
         Map<String, String> myCrazyEntity = Maps.newHashMap();
         myCrazyEntity.put("a", "alfa");
         myCrazyEntity.put("b", "beta");
+
+        Map elasticResponse = restTemplate.getForObject(elasticsearchUrl + "/" + trackId, Map.class);
+
+        Map<String, Object> myResponse = Maps.newHashMap((Map) elasticResponse.get("_source"));
+        myResponse.put("_id", elasticResponse.get("_id"));
 
         return ResponseEntity
                 .ok()
                 .header("Cache", "no-cache")
                 .header("header2", "value2")
-                .body(myCrazyEntity)
+                .body(myResponse)
             ;
     }
 
